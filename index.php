530496f8eca3b1a1a444794998534d41bce4316c
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-01-10 09:58:41 -0300

    refactor: separate data

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
+<?php 
+    $topics = [
+        [
+            'title' => 'Arrays',
+            'description' => 'How arrays and associative arrays work in PHP',
+            'difficulty' => 'Easy'
+        ],
+        [
+            'title' => 'Functions',
+            'description' => 'How to create functions in PHP',
+            'difficulty' => 'Easy'
+        ],
+        [
+            'title' => 'Objects',
+            'description' => 'How to declare and use objects in PHP',
+            'difficulty' => 'Easy'
+        ],
+        [
+            'title' => 'Databases',
+            'description' => 'How to connect to databases in PHP',
+            'difficulty' => 'Easy'
+        ]
+    ]
+?>
 <html>
 <head>
     <title>PHP Project</title>
 </head>
 <body>
     <h1>PHP Project</h1>
-    <?php 
-        $topics = [
-            [
-                'title' => 'Arrays',
-                'description' => 'How arrays and associative arrays work in PHP',
-                'difficulty' => 'Easy'
-            ],
-            [
-                'title' => 'Functions',
-                'description' => 'How to create functions in PHP',
-                'difficulty' => 'Easy'
-            ],
-            [
-                'title' => 'Objects',
-                'description' => 'How to declare and use objects in PHP',
-                'difficulty' => 'Easy'
-            ],
-            [
-                'title' => 'Databases',
-                'description' => 'How to connect to databases in PHP',
-                'difficulty' => 'Easy'
-            ]
-        ]
-    ?>
     <ul>
         <?php foreach($topics as $topic) : ?>
             <li>
