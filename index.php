a049db4619191348411c5e21ac119b289dea2379
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-01-10 09:26:50 -0300

    feat: demonstrate associative arrays

--- index.php
+++ index.php
@@ -4,5 +4,43 @@
 </head>
 <body>
     <h1>PHP Project</h1>
+    <?php 
+        $topics = [
+            [
+                'title' => 'Arrays',
+                'description' => 'How arrays and associative arrays work in PHP',
+                'difficulty' => 'Easy'
+            ],
+            [
+                'title' => 'Functions',
+                'description' => 'How to create functions in PHP',
+                'difficulty' => 'Easy'
+            ],
+            [
+                'title' => 'Objects',
+                'description' => 'How to declare and use objects in PHP',
+                'difficulty' => 'Easy'
+            ],
+            [
+                'title' => 'Databases',
+                'description' => 'How to connect to databases in PHP',
+                'difficulty' => 'Easy'
+            ]
+        ]
+    ?>
+    <ul>
+        <?php foreach($topics as $topic) : ?>
+            <li>
+                <h3>
+                    <?php echo $topic['title'] ?>
+                </h3>
+                <p>
+                    <?php echo $topic['description'] ?>
+                </p>
+                <p>
+                    <?php echo $topic['difficulty'] ?>
+                </p>
+            </li>
+        <?php endforeach ?>
 </body>
 </html>
