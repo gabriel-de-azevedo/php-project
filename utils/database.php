f2ee3cad5812ed9bbdba3def5582ec24da22094c
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2025-01-13 13:08:23 -0300

    fix: update database columns

--- utils/database.php
+++ utils/database.php
@@ -3,7 +3,7 @@
 function fetchPageData($pagePath) {
     $pdo = new PDO('sqlite:sqlite.db');
 
-    $selectQuery = "SELECT heading, content FROM pages WHERE \"path\" = '$pagePath'";
+    $selectQuery = "SELECT heading, content FROM pages WHERE url = '$pagePath'";
 
     $homePageData = $pdo->query($selectQuery)->fetch(PDO::FETCH_ASSOC);
 
